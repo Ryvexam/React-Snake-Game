33ae14b55d56182d6cd9f03e9f7e77dc088bee48
Ryvexam <maximevery@ryvexam.fr>
2024-06-27 16:25:48 +0200

    set server port to 80

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.json());  // Use this instead of bodyParser
 app.use(express.static(path.join(__dirname, 'public')));
