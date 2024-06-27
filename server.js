1bc40c1e375f267b608db2add04c2db57bb47253
Ryvexam <maximevery@ryvexam.fr>
2024-06-27 16:35:37 +0200

    test server

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.post('/api/score', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:${port}`);
 });
